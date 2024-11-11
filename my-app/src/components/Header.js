96f4fb2471126d0ab4f9e9b7e8cbcba87770b4b9
AdamJednacz <adamjednacz4@gmail.com>
2024-11-11 22:27:08 +0100

    switch

--- my-app/src/components/Header.js
+++ my-app/src/components/Header.js
@@ -60,22 +60,61 @@ const Header = ({pageType, isDarkMode, toggleDarkMode}) => {
                 </div>
                 <nav className={`menu ${isOpen ? 'open' : ''}`}>
                     <LinkR to="/" smooth={true} duration={500} onClick={handleCloseMenu}
-                          className="menu_item">Strona Główna</LinkR>
+                           className="menu_item">Strona Główna</LinkR>
                     <LinkR to="/o_nas" smooth={true} duration={500} onClick={handleCloseMenu}
-                          className="menu_item">O nas</LinkR>
+                           className="menu_item">O nas</LinkR>
                     <LinkR to="/grafik" smooth={true} duration={500} onClick={handleCloseMenu}
                            className="menu_item">Grafik</LinkR>
-                   <Link to="trenerzy" smooth={true} duration={500} onClick={() => { handleNavigateAndScroll('trainers'); handleCloseMenu(); }}
-                          className="menu_item">Trenerzy</Link>
 
-                    <Link to="cennik" smooth={true} duration={500} onClick={() => { handleNavigateAndScroll('pricing'); handleCloseMenu(); }}
+
+                    <Link to="cennik" smooth={true} duration={500} onClick={() => {
+                        handleNavigateAndScroll('pricing');
+                        handleCloseMenu();
+                    }}
                           className="menu_item">Cennik</Link>
-                    <Link to="kontakt" smooth={true} duration={500} onClick={() => { handleNavigateAndScroll('contact'); handleCloseMenu(); }}
+                    <Link to="kontakt" smooth={true} duration={500} onClick={() => {
+                        handleNavigateAndScroll('contact');
+                        handleCloseMenu();
+                    }}
                           className="menu_item">Kontakt</Link>
                 </nav>
-                <Link to="kontakt" smooth={true} duration={500} onClick={() => { handleNavigateAndScroll('contact'); handleCloseMenu(); }}
-                      ><button>Dołącz</button></Link>
-                <button onClick={toggleDarkMode}>{isDarkMode ? 'Light Mode' : 'Dark Mode'}</button>
+                <div className="buttons">
+                <Link className="contact_link" to="kontakt" smooth={true} duration={500} onClick={() => {
+                    handleNavigateAndScroll('contact');
+                    handleCloseMenu();
+                }}
+                >
+                    <button className="contact">Dołącz</button>
+                </Link>
+                <label htmlFor="theme" className={`theme ${isOpen ? 'open' : ''}`}>
+
+                    <span className="theme__toggle-wrap ">
+                        <input
+                            id="theme"
+                            className="theme__toggle"
+                            type="checkbox"
+                            role="switch"
+                            name="theme"
+                            value="dark"
+                            checked={isDarkMode}  // Bind to the dark mode state
+                            onChange={toggleDarkMode}  // Toggle the dark mode on change
+                        />
+                        <span className="theme__fill"></span>
+                        <span className="theme__icon">
+                            <span className="theme__icon-part"></span>
+                            <span className="theme__icon-part"></span>
+                            <span className="theme__icon-part"></span>
+                            <span className="theme__icon-part"></span>
+                            <span className="theme__icon-part"></span>
+                            <span className="theme__icon-part"></span>
+                            <span className="theme__icon-part"></span>
+                            <span className="theme__icon-part"></span>
+                            <span className="theme__icon-part"></span>
+                        </span>
+                    </span>
+
+                </label>
+                </div>
             </div>
         </header>
     );
